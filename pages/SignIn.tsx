5fb2f7923a9de68012618326e064845d9910225d
Isaac Zhu <iz@dhcp-128-189-229-71.ubcsecure.wireless.ubc.ca>
2023-10-05 20:08:03 -0700

    added new sign in page

--- pages/SignIn.tsx
+++ pages/SignIn.tsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, Image, TextInput, Button, StyleSheet, ActivityIndicator, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import Logo from '../assets/Sign-in-logo.svg'
+import Google from '../icons/Google.svg'
+import Facebook from '../icons/facebook-logo.svg'
+import { LinearGradient } from 'expo-linear-gradient';
+import { color } from 'react-native-elements/dist/helpers';
 
 const styles = StyleSheet.create({
     title: {
         marginBottom: 100
     },
     container: {
-        flex: 1,
-        padding: 24,
-        justifyContent: 'center',
+        // flex: 1,
+        // padding: 24,
+        display: 'flex',
+        flexDirection: 'column',
+        paddingBottom: 68,
+        justifyContent: 'flex-end',
         alignItems: 'center',
+        height: '100%',
+        margin: 'auto',
     },
     inputField: {
         borderColor: '#ccc',
@@ -23,8 +33,13 @@ const styles = StyleSheet.create({
         width: '80%',
         height: 40,
     },
-    button: {
-        width: '80%',
+    headerImage: {
+        width: 166,
+        height: 182,
+    },
+    text: {
+        fontSize: 14,
+        color: '#5F9FFF'
     }
 });
 
@@ -62,18 +77,67 @@ const SignIn = ({ refetch }: ISignIn) => {
                 <ActivityIndicator />
             ) : (
                 <>
-                    <Text style={styles.title}>Welcome to HomeBuds!</Text>
-                    <Text>To sign in, please input your email</Text>
-                    <TextInput
+                    <Logo width='166' height='182' style={{marginBottom: 108}}/>
+                    {/* <Text>To sign in, please input your email</Text> */}
+                    {/* <TextInput
                         style={styles.inputField}
                         value={email}
                         onChangeText={setEmail}
                         keyboardType='email-address'
                         autoCapitalize='none'
-                    />
+                        />
                     <View style={styles.button}>
                         <Button title="Sign In" onPress={handleSignIn} />
-                    </View>
+                    </View> */}
+                    <Pressable style={{
+                    width: 267,
+                    height: 53,
+                    flexShrink: 0,
+                    backgroundColor: 'white',
+                    display: 'flex',
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                    borderRadius: 16,
+                    flexDirection: 'row',
+                    marginBottom: 16
+                    }}
+                    onPress={() => {}}
+                    >
+                        <Google width={'25'} height={'25'} style={{marginLeft: 16, marginRight: 24}}/>
+                        <Text style={{
+                            fontSize: 15, 
+                            fontWeight: '600',
+                            // letterSpacing: -0.3
+                        }}>
+                            Sign in with Google
+                        </Text>
+                    </Pressable>
+                    <Pressable style={{
+                    width: 267,
+                    height: 53,
+                    flexShrink: 0,
+                    backgroundColor: 'white',
+                    display: 'flex',
+                    alignItems: "center",
+                    justifyContent: "flex-start",
+                    borderRadius: 16,
+                    flexDirection: 'row',
+                    marginBottom: 80,
+                    }}
+                    onPress={() => {}}
+                    >
+                        <Facebook width={'25'} height={'25'} style={{marginLeft: 16, marginRight: 24}}/>
+                        <Text style={{
+                            fontSize: 15, 
+                            fontWeight: '600',
+                            // letterSpacing: -0.3
+                        }}>
+                            Sign in with Facebook
+                        </Text>
+                    </Pressable>
+                    <Pressable onPress={()=>{}}>
+                        <Text style={styles.text}>I already have an account</Text>
+                    </Pressable>
                 </>
             )}
         </View>
